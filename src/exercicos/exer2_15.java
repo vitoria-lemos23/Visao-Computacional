d510a7e70ac1273f21ba16bcde964f11733db45b
Vitoria Lemos <vitoria.lemos183@gmail.com>
2025-06-14 20:50:08 -0300

    CRIAÇÃO DO ARQUIVO PARA O EXERCICIO 2.15 DO LIVRO PROCESSAMENTO DE IMAGENS DE GONZALEZ

---
+++ src/exercicos/exer2_15.java
@@ -0,0 +1,48 @@
+package exercicos;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class exer2_15 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int[][] s1 = new int[4][4];
+        int[][] s2 = new int[4][4];
+
+        ler(s1);
+        ler(s2);
+
+        imprimir(s1);
+        System.out.println();
+        imprimir(s2);
+
+    }
+
+
+    public static void ler(int matrix[][]) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("C:\\Users\\vitor\\IdeaProjects\\Visao-computacional\\src\\entrada.txt"));
+
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+    }
+
+
+    public static void imprimir(int matrix[][]) throws FileNotFoundException {
+
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                System.out.print( matrix[i][j]);
+            }
+        }
+        System.out.println();
+    }
+
+
+}
